b2f78371b9426602dfd7049e5d278ee53099a708
John Studarus <john@jhlconsulting.com>
2019-03-25 22:33:24 +0000

    hardcoded password for use by console logins

--- terraform/ubuntu_18_04.sh
+++ terraform/ubuntu_18_04.sh
@@ -7,3 +7,6 @@ apt-get update
 apt-get install -y bridge-utils debootstrap ifenslave ifenslave-2.6 \
   lsof chrony sudo tcpdump vlan python
 echo '8021q' >> /etc/modules-load.d/openstack-ansible.conf
+
+# openstack - for console login
+usermod --password Ym1Sx4Sc8lvEw root
