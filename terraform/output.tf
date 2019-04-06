9f2697a732c832f54ef312a9958019b136da4be9
John Studarus <john@jhlconsulting.com>
2019-04-06 01:54:56 +0000

    output host private IPs

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,11 +2,19 @@ output "Cloud ID Tag" {
   value = "${random_id.cloud.hex}"
 }
 
-output "Compute IPs" {
+output "Compute private IPs" {
+  value = "${packet_device.compute.*.access_private_ipv4}"
+}
+
+output "Compute public IPs" {
   value = "${packet_device.compute.*.access_public_ipv4}"
 }
 
-output "Control IPs" {
+output "Infra/Control private IPs" {
+  value = "${packet_device.control.*.access_private_ipv4}"
+}
+
+output "Infra/Control public IPs" {
   value = "${packet_device.control.*.access_public_ipv4}"
 }
 
