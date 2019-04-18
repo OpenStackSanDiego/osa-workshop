be1f4fd3a817832b5d0fb32c79c5d61478587033
John Studarus <john@jhlconsulting.com>
2019-04-18 22:25:33 +0000

    clean up code to support multiple infra and compute hosts add it back in at a later date

--- terraform/openstack_user_config.tf
+++ terraform/openstack_user_config.tf
@@ -1,49 +1,8 @@
-data "template_file" "infrastructure_hosts" {
-  depends_on = [
-    "packet_device.control",
-  ]
-
-  count = "${var.control_count}"
-
-  template = <<JSON
-$${hostname}:
-    ip: $${public_ip}
-JSON
-
- vars {
-    public_ip = "${element(packet_device.control.*.access_public_ipv4, count.index)}"
-    hostname  = "${element(packet_device.control.*.hostname, count.index)}"
-  }
-}
-
-data "template_file" "compute_hosts" {
-  depends_on = [
-    "packet_device.compute",
-  ]
-
-  count = "${var.compute_count}"
-
-  template = <<JSON
-$${hostname}:
-    ip: $${public_ip}
-JSON
-
-  vars {
-    public_ip = "${element(packet_device.compute.*.access_public_ipv4, count.index)}"
-    hostname  = "${element(packet_device.compute.*.hostname, count.index)}"
-  }
-}
 data "template_file" "openstack_user_config" {
   template = "${file("${path.module}/openstack_user_config-yml.tpl")}"
 
   vars {
     cloud_id = "${random_id.cloud.hex}"
-
-    infrastructure_hosts = "${join(",", data.template_file.infrastructure_hosts.*.rendered)}"
-    compute_hosts        = "${join(",", data.template_file.compute_hosts.*.rendered)}"
-
-    infra_hostnames    = "${jsonencode(join(",", packet_device.control.*.hostname))}"
-    compute_hostnames  = "${jsonencode(join(",", packet_device.control.*.hostname))}"
 
     control_public_addrs = "${join(",",packet_device.control.*.access_public_ipv4)}"
     compute_public_addrs = "${join(",",packet_device.compute.*.access_public_ipv4)}"
