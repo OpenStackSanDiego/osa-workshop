99a416a24d79408d75cd0a47bfb73c5222599c93
John Studarus <john@jhlconsulting.com>
2019-04-21 16:39:21 +0000

    generate public keys to be configured in deployed hosts
    removes the need for id_rsa

--- terraform/key.tf
+++ terraform/key.tf
@@ -11,6 +11,22 @@ resource "local_file" "private_key_pem" {
   filename   = "default.pem"
 }
 
+resource "local_file" "public_key_pem" {
+
+  depends_on = ["tls_private_key.default"]
+
+  content    = "${tls_private_key.default.public_key_pem}"
+  filename   = "default-pub.pem"
+}
+
+resource "local_file" "public_key_openssh" {
+
+  depends_on = ["tls_private_key.default"]
+
+  content    = "${tls_private_key.default.public_key_openssh}"
+  filename   = "default-openssh.pub"
+}
+
 resource "null_resource" "chmod" {
   depends_on = ["local_file.private_key_pem"]
 
