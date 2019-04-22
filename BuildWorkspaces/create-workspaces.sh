f39f6cbda343ec494e3094e4aa13d7d46a354836
John Studarus <john@jhlconsulting.com>
2019-04-22 22:30:44 +0000

    automation and sudo needed to delete/read default.pem

--- BuildWorkspaces/create-workspaces.sh
+++ BuildWorkspaces/create-workspaces.sh
@@ -32,7 +32,7 @@ do
   USER=$LAB_NAME$i
   echo "Creating $USER"
   #  encrypted password is openstack
-  sudo useradd -d /home/$USER -p 42ZTHaRqaaYvI -s /bin/bash $USER 
+  sudo useradd -d /home/$USER -p 42ZTHaRqaaYvI -s /bin/bash $USER -G sudo
   sudo mkdir /home/$USER
   sudo chown $USER.sudo /home/$USER
   sudo chmod 2775 /home/$USER
@@ -53,7 +53,7 @@ do
   sudo touch terraform.tfstate
   sudo chown $USER.sudo terraform.tfstate
   sudo -u $USER terraform init
-#  screen -dmS $USER-terraform-apply terraform apply -auto-approve
+  screen -dmS $USER-terraform-apply terraform apply -auto-approve
   popd
 done
 
