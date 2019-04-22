e30d3d9d683a7a6108d5701567ea72dab9cc3cb0
John Studarus <john@jhlconsulting.com>
2019-04-22 03:46:36 +0000

    keys are required for playbooks to run

--- terraform/osa-playbooks.sh
+++ terraform/osa-playbooks.sh
@@ -1,3 +1,5 @@
+eval `ssh-agent`
+ssh-add ~/.ssh/default.pem
 ( cd /opt/openstack-ansible ; ./scripts/pw-token-gen.py --file /etc/openstack_deploy/user_secrets.yml)
 cd /opt/openstack-ansible/playbooks/
 openstack-ansible setup-infrastructure.yml --syntax-check
